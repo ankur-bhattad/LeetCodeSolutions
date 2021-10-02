244c619a902c5c5cd99a7036f8016759209be203
Ankur-Bhattad <sunny8nits@yahoo.co.in>
2021-10-02 19:46:13 +0530

    Leet Code - Medium - Problem 11 - Container with most water Suprdaily test Visa test: can text be created by all characters of note

---
+++ src/suprdaily/SolutionOne.java
@@ -0,0 +1,48 @@
+package suprdaily;
+
+import java.util.*;
+
+public class SolutionOne {
+
+    public static void main(String args[]){
+        System.out.println(KFrequency("aabcd", 2));
+    }
+
+    static String KFrequency(String S, int K) {
+        // Write your code here
+        if (S == null || S.length() == 0) {
+            return "-1";
+        }
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for (int i=0; i<S.length(); i++) {
+            int count = 0;
+            if (charCountMap.containsKey(S.charAt(i))) {
+                count = charCountMap.get(S.charAt(i)) + 1;
+            } else {
+                count = 1;
+            }
+            charCountMap.put(S.charAt(i), count);
+        }
+
+        Map<Integer, TreeSet<Character>> frequencyCharMapping = new TreeMap<>();
+        charCountMap.forEach((character, count) -> {
+            if (!(frequencyCharMapping.containsKey(count))) {
+                TreeSet<Character> charSet = new TreeSet<>();
+                frequencyCharMapping.put(count, charSet);
+            }
+            frequencyCharMapping.get(count).add(character);
+        });
+
+        if (frequencyCharMapping.size() < K) {
+            return "-1";
+        } else {
+            int size = frequencyCharMapping.size();
+            int index = size - K;
+            ArrayList<Integer> keys = new ArrayList<>(frequencyCharMapping.keySet());
+            int frequency = keys.get(index);
+            Set<Character> charSet = frequencyCharMapping.get(frequency);
+            return charSet.stream().findFirst().get().toString();
+        }
+    }
+}
---
+++ src/visa/SolSix.java
@@ -0,0 +1,52 @@
+package visa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class SolSix {
+    public static void main (String args[]) {
+        String text = "abcdefghijklmnopqrstuvwxyz";
+        String note = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+        System.out.println(canConstructLetter(text, note));
+    }
+
+    public static boolean canConstructLetter(String text, String note) {
+        // Write your code here
+        System.out.println(text+":"+note);
+        if (text == null || note == null) {
+            return false;
+        }
+        note = note.replaceAll(" ","");
+        if (text.length() < note.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> charCountInText = new HashMap<>();
+        for (int i = 0; i<text.length(); i++) {
+            Character c = text.charAt(i);
+            int count = 1;
+            if (charCountInText.containsKey(c)) {
+                count = charCountInText.get(c)+1;
+            }
+            charCountInText.put(c, count);
+        }
+
+        for (int i=0; i<note.length(); i++) {
+            Character c = note.charAt(i);
+            if (c!=' ') {
+                int count = 0;
+                if (charCountInText.containsKey(c)) {
+                    count = charCountInText.get(c);
+                    if (count == 1) {
+                        charCountInText.remove(c);
+                    } else {
+                        charCountInText.put(c, count-1);
+                    }
+                } else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
