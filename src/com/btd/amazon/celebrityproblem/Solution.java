fc36b8241818c62c3a635b61bea5e10a1c291e7b
Ankur-Bhattad <sunny8nits@yahoo.co.in>
2021-10-20 10:16:40 +0530

    Solution to Celebrity Problem asked in amazon interview

---
+++ src/com/btd/amazon/celebrityproblem/Solution.java
@@ -0,0 +1,52 @@
+package com.btd.amazon.celebrityproblem;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] celebs = {"a", "b", "c"};
+        System.out.println(findCelebrity(celebs));
+    }
+
+    private static String findCelebrity(String[] celebs) {
+
+        if (celebs.length <= 1) {
+            return null;
+        }
+        int i = 0;
+        int j = celebs.length-1;
+
+        while (i < j) {
+            if (knows(i, j)) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        // i is celebrity if itmeets criteria that he knows no one and everyone else knows him
+        int count = 0;
+        for (int k = 0; k < celebs.length; k++) {
+            if (k != i) {
+                if (knows(i, k) || !knows(k, i)) {
+                    break;
+                } else {
+                    count++;
+                }
+            }
+        }
+
+        if (count == celebs.length - 1) {
+            return celebs[i];
+        }
+
+        return  null;
+    }
+
+    private static boolean knows(int i, int j) {
+        // C is celebrity
+        int [][] familiarityMatrix = { {0, 0, 1}, {1, 0, 1}, {0, 0, 0}};
+        if (familiarityMatrix[i][j] == 1) {
+            return true;
+        }
+        return false;
+    }
+}
